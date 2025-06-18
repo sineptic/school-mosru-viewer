4d8266d4c0dd4888cd9cec6e876870b6f8aea3fc
sineptic <sineptic0@gmail.com>
2025-06-18 18:21:27 +0300

    first working version

--- parsing/src/main.rs
+++ parsing/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, time::Duration};
+
+use chromiumoxide::{Browser, BrowserConfig};
+use futures::StreamExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let (browser, mut handler) =
+        Browser::launch(BrowserConfig::builder().with_head().build().unwrap()).await?;
+
+    let handler = tokio::spawn(async move {
+        while handler.next().await.is_some() {
+            //
+        }
+    });
+
+    let page = browser.new_page("https://school.mos.ru").await?;
+    tokio::time::sleep(Duration::from_millis(1000)).await;
+    page.find_element(".style_btn__3lIWs")
+        .await?
+        .click()
+        .await?;
+
+    handler.await?;
+    println!("Interactive mode");
+    std::thread::sleep_ms(1_000_000);
+
+    Ok(())
 }
