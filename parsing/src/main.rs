53114a2d198fdfcf0deae584db20999f0831946a
sineptic <sineptic0@gmail.com>
2025-06-19 19:33:18 +0300

    implement first api request

--- parsing/src/main.rs
+++ parsing/src/main.rs
@@ -1,67 +1,30 @@
-use std::time::Duration;
-
-use chromiumoxide::{Browser, BrowserConfig, Page};
-use futures::StreamExt;
-
-async fn new_school_mosru_page(browser: &Browser) -> anyhow::Result<Page> {
-    let page = browser.new_page("https://school.mos.ru").await?;
-    tokio::time::sleep(Duration::from_secs(4)).await;
-    if let Ok(login_button) = page.find_element(".style_btn__3lIWs").await {
-        let login = std::env::var("MOSRU_LOGIN")?;
-        let password = std::env::var("MOSRU_PASSWORD")?;
-
-        login_button.click().await?;
-
-        tokio::time::sleep(Duration::from_secs(1)).await;
-        let url = page.url().await.unwrap().unwrap();
-        if url.starts_with("https://login.mos.ru") {
-            println!("login required");
-            tokio::time::sleep(Duration::from_secs(2)).await;
-            page.find_element("#login")
-                .await
-                .unwrap()
-                .click()
-                .await?
-                .type_str(login)
-                .await?;
-            page.find_element("#password")
-                .await
-                .unwrap()
-                .click()
-                .await?
-                .type_str(password)
-                .await?;
-            page.find_element("#bind").await.unwrap().click().await?;
-            tokio::time::sleep(Duration::from_secs(4)).await;
-        }
-    }
-    tokio::time::sleep(Duration::from_secs(10)).await;
-    tokio::time::sleep(LOAD_TIME).await;
-    Ok(page)
-}
-
-const LOAD_TIME: Duration = Duration::from_secs(5);
+use reqwest::header::HeaderMap;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv()?;
-    let (mut browser, mut handler) =
-        Browser::launch(BrowserConfig::builder().with_head().build().unwrap()).await?;
+    let student_id = 31823383;
+    let url =
+        format!("https://school.mos.ru/api/family/web/v1/subject_marks?student_id={student_id}");
+    // let response = reqwest::get(url).await;
+    let authorization = std::env::var("MOSRU_BEARER").unwrap();
+    let client = reqwest::ClientBuilder::new()
+        .default_headers(HeaderMap::from_iter([
+            (
+                reqwest::header::AUTHORIZATION,
+                format!("Bearer {authorization}").parse().unwrap(),
+            ),
+            (
+                "x-mes-subsystem".parse().unwrap(),
+                "familyweb".parse().unwrap(),
+            ),
+        ]))
+        .build()
+        .unwrap();
 
-    let handler = tokio::spawn(async move {
-        while handler.next().await.is_some() {
-            //
-        }
-    });
-
-    let page = new_school_mosru_page(&browser).await?;
-    println!("page loaded");
-
-    // marks at https://school.mos.ru/api/family/web/v1/subject_marks?student_id=*****
-    //
-    //
-    handler.await?;
-    browser.close().await?;
+    let request = client.get(url).build().unwrap();
+    let response = client.execute(request).await?;
+    println!("{}", response.text().await?);
 
     Ok(())
 }
