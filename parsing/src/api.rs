b2f1adea2024fc232e1ecd41b22f217206b4e29f
sineptic <sineptic0@gmail.com>
2025-06-20 20:32:07 +0300

    create api client

--- parsing/src/api.rs
+++ parsing/src/api.rs
@@ -1,9 +1,53 @@
-use reqwest::Url;
+use reqwest::{Client, Method, Url, header::HeaderMap};
+use serde::de::DeserializeOwned;
+
+pub struct ApiClient {
+    client: Client,
+}
+impl ApiClient {
+    pub fn new(authorization_token: impl AsRef<str>) -> Self {
+        Self {
+            client: reqwest::ClientBuilder::new()
+                .default_headers(HeaderMap::from_iter([
+                    (
+                        reqwest::header::AUTHORIZATION,
+                        format!("Bearer {}", authorization_token.as_ref())
+                            .parse()
+                            .unwrap(),
+                    ),
+                    (
+                        "x-mes-subsystem".parse().unwrap(),
+                        "familyweb".parse().unwrap(),
+                    ),
+                ]))
+                .build()
+                .unwrap(),
+        }
+    }
+    pub async fn trigger_endpoint<E: ApiEndpoint>(
+        &self,
+        endpoint: E,
+    ) -> anyhow::Result<E::ProcessedResponse> {
+        let response = self
+            .client
+            .request(E::METHOD, endpoint.url())
+            .send()
+            .await?
+            .json::<E::RawResponse>()
+            .await?;
+        Ok(E::transform_response(response))
+    }
+}
 
 pub trait ApiEndpoint: Into<Url> {
     fn url(self) -> Url {
         self.into()
     }
+    const METHOD: Method;
+
+    type RawResponse: DeserializeOwned;
+    type ProcessedResponse;
+    fn transform_response(raw_response: Self::RawResponse) -> Self::ProcessedResponse;
 }
 
 pub struct Marks {
@@ -19,7 +63,17 @@ impl From<Marks> for Url {
         .unwrap()
     }
 }
-impl ApiEndpoint for Marks {}
+impl ApiEndpoint for Marks {
+    const METHOD: Method = Method::GET;
+    type RawResponse = crate::raw_types::marks::Marks;
+    type ProcessedResponse = (
+        Vec<crate::types::marks::Subject>,
+        Vec<crate::types::marks::Mark>,
+    );
+    fn transform_response(raw_response: Self::RawResponse) -> Self::ProcessedResponse {
+        crate::types::marks::extract_marks_info(raw_response)
+    }
+}
 
 pub struct Schedule {
     pub student_id: u64,
@@ -37,21 +91,32 @@ impl From<Schedule> for Url {
         .unwrap()
     }
 }
-impl ApiEndpoint for Schedule {}
-
-pub struct Homework {
-    pub student_id: u64,
-    pub from: String,
-    pub to: String,
-}
-impl From<Homework> for Url {
-    fn from(value: Homework) -> Self {
-        format!(
-            "https://school.mos.ru/api/family/web/v1/homeworks?from={}&to={}&student_id={}",
-            value.from, value.to, value.student_id,
-        )
-        .parse()
-        .unwrap()
+impl ApiEndpoint for Schedule {
+    const METHOD: Method = Method::GET;
+    type RawResponse = crate::raw_types::schedule::Schedule;
+    type ProcessedResponse = Vec<crate::types::schedule::LessonSchedule>;
+    fn transform_response(raw_response: Self::RawResponse) -> Self::ProcessedResponse {
+        raw_response
+            .payload
+            .into_iter()
+            .flat_map(crate::types::schedule::transform)
+            .collect()
     }
 }
-impl ApiEndpoint for Homework {}
+
+// pub struct Homework {
+//     pub student_id: u64,
+//     pub from: String,
+//     pub to: String,
+// }
+// impl From<Homework> for Url {
+//     fn from(value: Homework) -> Self {
+//         format!(
+//             "https://school.mos.ru/api/family/web/v1/homeworks?from={}&to={}&student_id={}",
+//             value.from, value.to, value.student_id,
+//         )
+//         .parse()
+//         .unwrap()
+//     }
+// }
+// impl ApiEndpoint for Homework {}
--- parsing/src/main.rs
+++ parsing/src/main.rs
@@ -1,51 +1,22 @@
-use api::ApiEndpoint;
-use reqwest::header::HeaderMap;
+use api::ApiClient;
 
 mod api;
-
-fn api_reqwest_client(authorization_token: impl AsRef<str>) -> reqwest::Client {
-    reqwest::ClientBuilder::new()
-        .default_headers(HeaderMap::from_iter([
-            (
-                reqwest::header::AUTHORIZATION,
-                format!("Bearer {}", authorization_token.as_ref())
-                    .parse()
-                    .unwrap(),
-            ),
-            (
-                "x-mes-subsystem".parse().unwrap(),
-                "familyweb".parse().unwrap(),
-            ),
-        ]))
-        .build()
-        .unwrap()
-}
-
 mod raw_types;
 mod types;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv()?;
-    let client = api_reqwest_client(std::env::var("MOSRU_BEARER").unwrap());
+    let client = ApiClient::new(std::env::var("MOSRU_BEARER").unwrap());
 
     let student_id = 31823383;
-    let response = client
-        .get(
-            api::Homework {
-                student_id,
-                from: "2024-09-01".into(),
-                to: "2025-06-22".into(),
-            }
-            .url(),
-        )
-        .send()
-        .await?
-        .json::<raw_types::homework::Root>()
-        .await?;
+    let endpoint = api::Schedule {
+        student_id,
+        dates: vec!["2024-09-02".into()],
+    };
+    let response = client.trigger_endpoint(endpoint).await?;
 
-    println!("{response:#?}");
-    // println!("{}", response.payload.len());
+    println!("{}", serde_json::to_string_pretty(&response).unwrap());
 
     Ok(())
 }
