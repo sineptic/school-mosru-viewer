d984e8775ff2b52a6e9a8a80e3e2c642d5e0d40e
sineptic <sineptic0@gmail.com>
2025-06-22 19:37:47 +0300

    implement FromStr for Date and Time

--- parsing/src/time.rs
+++ parsing/src/time.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -8,17 +8,15 @@ pub struct Date {
     month: u64,
     day: u64,
 }
-impl<'de> Deserialize<'de> for Date {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let err = || serde::de::Error::custom("Date format should be 'year-mm-dd'.");
-        let input = String::deserialize(deserializer)?;
-        if input.len() != 10 {
-            return Err(err());
+impl FromStr for Date {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const ERR: &str = "Date format should be 'year-mm-dd'.";
+        if s.len() != 10 {
+            return Err(ERR);
         }
-        let [y1, y2, y3, y4, dash1, m1, m2, dash2, d1, d2] = input.as_bytes().try_into().unwrap();
+        let [y1, y2, y3, y4, dash1, m1, m2, dash2, d1, d2] = s.as_bytes().try_into().unwrap();
         if !(y1.is_ascii_digit()
             && y2.is_ascii_digit()
             && y3.is_ascii_digit()
@@ -30,7 +28,7 @@ impl<'de> Deserialize<'de> for Date {
             && d1.is_ascii_digit()
             && d2.is_ascii_digit())
         {
-            return Err(err());
+            return Err(ERR);
         }
         let year = (y1 - b'0') as u64 * 1000
             + (y2 - b'0') as u64 * 100
@@ -39,10 +37,19 @@ impl<'de> Deserialize<'de> for Date {
         let month = (m1 - b'0') as u64 * 10 + (m2 - b'0') as u64;
         let day = (d1 - b'0') as u64 * 10 + (d2 - b'0') as u64;
         if !((1..=12).contains(&month) && (1..=31).contains(&day)) {
-            return Err(err());
+            return Err(ERR);
         }
 
         Ok(Self { year, month, day })
+    }
+}
+impl<'de> Deserialize<'de> for Date {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let input = String::deserialize(deserializer)?;
+        input.parse().map_err(serde::de::Error::custom)
     }
 }
 impl Display for Date {
@@ -64,32 +71,39 @@ pub struct Time {
     hours: u64,
     minutes: u64,
 }
-impl<'de> Deserialize<'de> for Time {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let err = || serde::de::Error::custom("Time format should be 'hh:mm'.");
-        let input = String::deserialize(deserializer)?;
-        if input.len() != 5 {
-            return Err(err());
+impl FromStr for Time {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const ERR: &str = "Time format should be 'hh:mm'.";
+        if s.len() != 5 {
+            return Err(ERR);
         }
-        let [h1, h2, column, m1, m2] = input.as_bytes().try_into().unwrap();
+        let [h1, h2, column, m1, m2] = s.as_bytes().try_into().unwrap();
         if !(h1.is_ascii_digit()
             && h2.is_ascii_digit()
             && column == b':'
             && m1.is_ascii_digit()
             && m2.is_ascii_digit())
         {
-            return Err(err());
+            return Err(ERR);
         }
         let hours = (h1 - b'0') as u64 * 10 + (h2 - b'0') as u64;
         let minutes = (m1 - b'0') as u64 * 10 + (m2 - b'0') as u64;
         if !((0..24).contains(&hours) && (0..60).contains(&minutes)) {
-            return Err(err());
+            return Err(ERR);
         }
 
         Ok(Self { hours, minutes })
+    }
+}
+impl<'de> Deserialize<'de> for Time {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let input = String::deserialize(deserializer)?;
+        input.parse().map_err(serde::de::Error::custom)
     }
 }
 impl Display for Time {
