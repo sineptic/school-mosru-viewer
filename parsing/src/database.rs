755245dcb396a47c532fe2b31e56a5ec6ce322e1
sineptic <sineptic0@gmail.com>
2025-07-15 11:14:03 +0300

    style: add context for errors

--- parsing/src/database.rs
+++ parsing/src/database.rs
@@ -15,17 +15,22 @@ pub struct MutDatabase<'a> {
 }
 impl Database {
     pub fn open() -> Result<Self> {
-        let connection = Connection::open("db.sqlite")?;
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS additional_homework_materials (
+        let file_name = "db.sqlite";
+        let connection = Connection::open(file_name)
+            .with_context(|| format!("Failed to open database at `{file_name}`"))?;
+        connection
+            .execute(
+                "CREATE TABLE IF NOT EXISTS additional_homework_materials (
                 id    TEXT PRIMARY KEY,
                 title TEXT,
                 urls  TEXT NOT NULL
             )",
-            (),
-        )?;
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS homeworks (
+                (),
+            )
+            .context("Error in SQL that creates additional_homework_materials table")?;
+        connection
+            .execute(
+                "CREATE TABLE IF NOT EXISTS homeworks (
                 id                   INTEGER PRIMARY KEY,
                 task                 TEXT NOT NULL,
                 subject_name         TEXT NOT NULL,
@@ -35,21 +40,23 @@ impl Database {
                 date_prepared_for    TEXT NOT NULL,
                 additional_materials TEXT NOT NULL
             )",
-            (),
-        )?;
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS lesson_schedules (
+                (),
+            )
+            .context("Error in SQL that creates homeworks table")?;
+        connection
+            .execute(
+                "CREATE TABLE IF NOT EXISTS lesson_schedules (
                 subject_name      TEXT NOT NULL,
                 room_number       TEXT,
                 date              TEXT NOT NULL,
                 begin_time        TEXT NOT NULL,
                 end_time          TEXT NOT NULL,
                 absence_reason_id INTEGER,
-                schedule_item_id  INTEGER NOT NULL,
-                UNIQUE(schedule_item_id)
+                schedule_item_id  INTEGER PRIMARY KEY
             )",
-            (),
-        )?;
+                (),
+            )
+            .context("Error in SQL that creates lesson_schedules table")?;
         Ok(Self { connection })
     }
     pub fn start_transaction(&mut self) -> Result<MutDatabase<'_>> {
@@ -64,9 +71,11 @@ impl Database {
         &mut self,
         handle: impl FnOnce(&mut MutDatabase) -> anyhow::Result<()>,
     ) -> Result<()> {
-        let mut tr = self.start_transaction()?;
-        handle(&mut tr)?;
-        tr.commit()?;
+        let mut tr = self
+            .start_transaction()
+            .context("Failed to start transaction")?;
+        handle(&mut tr).context("Failed to run transaction body")?;
+        tr.commit().context("Failed to commit transaction")?;
         Ok(())
     }
 }
@@ -91,10 +100,13 @@ impl MutDatabase<'_> {
         &self,
         materials: Vec<AdditionalMaterial>,
     ) -> Result<()> {
-        let mut stmt = self.transaction.prepare_cached(
-            "INSERT OR IGNORE INTO additional_homework_materials (id, title, urls)
+        let mut stmt = self
+            .transaction
+            .prepare_cached(
+                "INSERT OR IGNORE INTO additional_homework_materials (id, title, urls)
                 VALUES (?1, ?2, ?3)",
-        )?;
+            )
+            .context("SQL syntax error in expression that inserts additional_homework_materials")?;
         for material in materials {
             stmt.execute((material.id, material.title, serialize(material.urls)))
                 .context("Failed to store homework additional material")?;
@@ -106,7 +118,8 @@ impl MutDatabase<'_> {
         let mut stmt = self.transaction.prepare_cached(
             "INSERT OR IGNORE INTO homeworks (id, task, subject_name, created_at, updated_at, assigned_on, date_prepared_for, additional_materials)
                     VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)",
-                    )?;
+        )
+        .context("SQL syntax error in expression that inserts homeworks")?;
 
         let (additional_materials, other): (Vec<_>, Vec<_>) = hws
             .into_iter()
@@ -135,7 +148,8 @@ impl MutDatabase<'_> {
 
         self.store_homework_additional_materials(
             additional_materials.into_iter().flatten().collect(),
-        )?;
+        )
+        .context("Failed to store additional materials for homeworks")?;
 
         for hw in other {
             stmt.execute(hw).context("Failed to store homework")?;
@@ -147,7 +161,8 @@ impl MutDatabase<'_> {
         let mut stmt = self.transaction.prepare_cached(
             "INSERT OR IGNORE INTO lesson_schedules (subject_name, room_number, date, begin_time, end_time, absence_reason_id, schedule_item_id)
                 VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7)"
-        )?;
+        )
+        .context("SQL syntax error in expression that inserts lesson_schedules")?;
         for lesson_schedule in lesson_schedules {
             stmt.execute((
                 lesson_schedule.subject_name,
@@ -157,75 +172,76 @@ impl MutDatabase<'_> {
                 lesson_schedule.end_time,
                 lesson_schedule.absence_reason_id,
                 lesson_schedule.schedule_item_id,
-            ))?;
+            ))
+            .context("Failed to add one of lesson schedules")?;
         }
         Ok(())
     }
 }
 
-impl Database {
-    pub fn get_homework_additional_material(
-        &self,
-        id: &str,
-    ) -> Result<Option<types::homework::AdditionalMaterial>> {
-        let mut stmt = self
-            .connection
-            .prepare("SELECT (id, title, urls) FROM additional_homework_materials WHERE id = ?1")?;
+// impl Database {
+//     pub fn get_homework_additional_material(
+//         &self,
+//         id: &str,
+//     ) -> Result<Option<types::homework::AdditionalMaterial>> {
+//         let mut stmt = self
+//             .connection
+//             .prepare("SELECT (id, title, urls) FROM additional_homework_materials WHERE id = ?1")?;
 
-        stmt.query_one([id], |x| {
-            Ok(AdditionalMaterial {
-                id: x.get_unwrap(0),
-                title: x.get_unwrap(1),
-                urls: {
-                    let string: String = x.get_unwrap(2);
-                    deserialize(string)
-                },
-            })
-        })
-        .optional()
-        .context("Failed to get homework additional material")
-    }
-    pub fn get_homework(&self, id: u64) -> Result<Option<Homework>> {
-        let mut stmt = self.connection.prepare(
-            "SELECT (id, task, entry_id, entry_student_id, created_at, updated_at, assigned_on, date_prepared_for, additional_materials) FROM homeworks WHERE id = ?1",
-        )?;
+//         stmt.query_one([id], |x| {
+//             Ok(AdditionalMaterial {
+//                 id: x.get_unwrap(0),
+//                 title: x.get_unwrap(1),
+//                 urls: {
+//                     let string: String = x.get_unwrap(2);
+//                     deserialize(string)
+//                 },
+//             })
+//         })
+//         .optional()
+//         .context("Failed to get homework additional material")
+//     }
+//     pub fn get_homework(&self, id: u64) -> Result<Option<Homework>> {
+//         let mut stmt = self.connection.prepare(
+//             "SELECT (id, task, entry_id, entry_student_id, created_at, updated_at, assigned_on, date_prepared_for, additional_materials) FROM homeworks WHERE id = ?1",
+//         )?;
 
-        let a = stmt
-            .query_one([id], |x| {
-                Ok((
-                    x.get_unwrap(0),
-                    x.get_unwrap(1),
-                    x.get_unwrap(2),
-                    x.get_unwrap(3),
-                    x.get_unwrap(4),
-                    x.get_unwrap(5),
-                    x.get_unwrap(6),
-                    deserialize::<Vec<String>>(x.get_unwrap(7)),
-                ))
-            })
-            .optional()
-            .context("Failed to store homework")?;
-        if let Some(a) = a {
-            Ok(Some(Homework {
-                id: a.0,
-                task: a.1,
-                subject_name: a.2,
-                created_at: a.3,
-                updated_at: a.4,
-                assigned_on: a.5,
-                date_prepared_for: a.6,
-                additional_materials: {
-                    a.7.into_iter()
-                        .map(|id| {
-                            self.get_homework_additional_material(&id)
-                                .transpose()
-                                .expect("All homework's additional materials should be added to db")
-                        })
-                        .collect::<Result<Vec<_>, _>>()?
-                },
-            }))
-        } else {
-            Ok(None)
-        }
-    }
-}
+//         let a = stmt
+//             .query_one([id], |x| {
+//                 Ok((
+//                     x.get_unwrap(0),
+//                     x.get_unwrap(1),
+//                     x.get_unwrap(2),
+//                     x.get_unwrap(3),
+//                     x.get_unwrap(4),
+//                     x.get_unwrap(5),
+//                     x.get_unwrap(6),
+//                     deserialize::<Vec<String>>(x.get_unwrap(7)),
+//                 ))
+//             })
+//             .optional()
+//             .context("Failed to store homework")?;
+//         if let Some(a) = a {
+//             Ok(Some(Homework {
+//                 id: a.0,
+//                 task: a.1,
+//                 subject_name: a.2,
+//                 created_at: a.3,
+//                 updated_at: a.4,
+//                 assigned_on: a.5,
+//                 date_prepared_for: a.6,
+//                 additional_materials: {
+//                     a.7.into_iter()
+//                         .map(|id| {
+//                             self.get_homework_additional_material(&id)
+//                                 .transpose()
+//                                 .expect("All homework's additional materials should be added to db")
+//                         })
+//                         .collect::<Result<Vec<_>, _>>()?
+//                 },
+//             }))
+//         } else {
+//             Ok(None)
+//         }
+//     }
+// }
